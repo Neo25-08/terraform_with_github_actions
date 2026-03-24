151a5444aa1745946ee15e3598264f367b984b15
Pratyush <pratyushprajapati1@gmail.com>
2026-03-24 08:36:14 +0545

    Policy Fix?

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,24 +6,33 @@ resource "aws_s3_bucket" "site" {
   bucket = var.bucket_name
 }
 
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_website_configuration" "site_config" {
   bucket = aws_s3_bucket.site.id
 
   index_document {
     suffix = "index.html"
   }
-}
 
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.site.id
-
-  block_public_acls   = false
-  block_public_policy = false
-  restrict_public_buckets = false
+  depends_on = [
+    aws_s3_bucket.site
+  ]
 }
 
 resource "aws_s3_bucket_policy" "public_read" {
   bucket = aws_s3_bucket.site.id
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.public_access
+  ]
 
   policy = jsonencode({
     Version = "2012-10-17"
